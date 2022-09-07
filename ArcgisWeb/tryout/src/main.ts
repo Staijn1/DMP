b53f78c49fd4ea1c5f154fd852ef3d5abb2d80d7
Staijn1 <stein@jnkr.eu>
2022-09-07 15:52:05 +0200

    It works! Map shows your current location

--- ArcgisWeb/tryout/src/main.ts
+++ ArcgisWeb/tryout/src/main.ts
@@ -1,17 +1,30 @@
-import ArcGISMap from "@arcgis/core/Map";
-import MapView from "@arcgis/core/views/MapView";
-import './style.css';
+import ArcGISMap from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
+import Track from '@arcgis/core/widgets/Track';
+import Graphic from '@arcgis/core/Graphic';
+
 const map = new ArcGISMap({
-  basemap: "streets-vector"
+  basemap: 'streets-vector'
 });
 
 const view = new MapView({
   map: map,
-  container: "viewDiv",
-  center: [-118.244, 34.052],
+  container: 'viewDiv',
+  center: [4.9041,52.3676],
   zoom: 12
 });
 
+const track = new Track({
+  view: view,
+  useHeadingEnabled: true,
+  goToOverride: function (view, options) {
+    options.target.scale = 1500;  // Override the default map scale
+    return view.goTo(options.target);
+  }
+})
+
+view.ui.add(track, 'top-left');
+
 view.when(() => {
-  console.log("Map is loaded");
+  console.log('Map is loaded');
 })
