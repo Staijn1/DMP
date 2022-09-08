54994cf13aa1e71b8a1343b01b371f566d6af244
Staijn1 <stein@jnkr.eu>
2022-09-08 14:48:49 +0200

    Moved camera to amsterdam

--- ArcgisWeb/tryout/src/pages/gebouwen.ts
+++ ArcgisWeb/tryout/src/pages/gebouwen.ts
@@ -1,86 +1,165 @@
-// Create a 3d scene with arcgis
-import Map from '@arcgis/core/Map';
+import WebScene from '@arcgis/core/WebScene';
 import SceneView from '@arcgis/core/views/SceneView';
-import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
+import SketchViewModel from '@arcgis/core/widgets/Sketch/SketchViewModel';
+import SceneLayer from '@arcgis/core/layers/SceneLayer';
+import esriConfig from '@arcgis/core/config.js';
+import {apiKey} from '../vite-env';
 
-const footprintBuildings = new FeatureLayer({
-  url:
-    'https://services2.arcgis.com/cFEFS0EWrhfDeVw9/arcgis/rest/services/san_francisco_footprints_selection/FeatureServer',
-  renderer: {
-    type: 'simple',
-    symbol: {
-      type: 'polygon-3d',
-      symbolLayers: [
-        {
-          type: 'fill',
-          material: {color: [255, 237, 204]},
-          outline: {color: [133, 108, 62, 0.5]}
-        }
-      ]
-    }
-  } as any,
-  visible: false
+esriConfig.apiKey = apiKey;
+
+const map = new WebScene({
+  basemap: 'arcgis-imagery',
+  ground: 'world-elevation'
 });
 
-const extrudedBuildings = new FeatureLayer({
-  url:
-    'https://services2.arcgis.com/cFEFS0EWrhfDeVw9/arcgis/rest/services/san_francisco_footprints_selection/FeatureServer',
-  renderer: {
-    type: 'simple',
-    symbol: {
-      type: 'polygon-3d',
-      symbolLayers: [
-        {
-          type: 'extrude',
-          material: {color: [255, 237, 204]},
-          edges: {
-            type: 'solid',
-            color: [133, 108, 62, 0.5],
-            size: 1
-          }
-        }
-      ]
-    },
-    visualVariables: [
-      {
-        type: 'size',
-        field: 'heightcm',
-        valueUnit: 'centimeters'
-      }
-    ]
-  } as any,
-  visible: true
-});
-
-
-// Create Map
-var map = new Map({
-  basemap: 'arcgis-light-gray',
-  ground: 'world-elevation',
-  layers: [footprintBuildings, extrudedBuildings]
-});
-
-// Create the SceneView
-var view = new SceneView({
+// Create the view
+const view = new SceneView({
   container: 'viewDiv',
   map: map,
   camera: {
-// @ts-ignore
-    position: [-122.39899666, 37.77940678, 314.88439],
-    heading: 356.82,
-    tilt: 78.61
+    position: {
+      latitude: 52.377956,
+      longitude: 4.897070,
+      z: 5000
+    },
+    tilt: 0,
+    heading: 0
   },
-  qualityProfile: 'high'
+  environment: {
+    lighting: {
+      date: new Date('June 15, 2015 16:00:00 CET'),
+      directShadowsEnabled: true,
+      ambientOcclusionEnabled: true
+    }
+  }
 });
-let type = 'cartographic';
 
-/*
+/********************************************************************
+ * Add layer containing street furniture features: benches, street lamps
+ ********************************************************************/
 
-document.getElementById("footprint").addEventListener("click", function (event) {
-  footprintBuildings.visible = true;
-  extrudedBuildings.visible = false;
+// convenience function to retrieve the WebStyleSymbols based on their name
+function getStreetSymbol(name: string) {
+  return {
+    type: 'web-style', // autocasts as new WebStyleSymbol()
+    name: name,
+    styleName: 'EsriRealisticStreetSceneStyle'
+  };
+}
+
+// use a UniqueValueRenderer to symbolize the different feature types (street lamps, trash bin)
+const streetFurnitureRenderer = {
+  type: 'unique-value', // autocasts as new UniqueValueRenderer()
+  field: 'CATEGORY',
+  defaultSymbol: getStreetSymbol('Light_On_Post_-_Light_on'),
+  uniqueValueInfos: [
+    {
+      value: 'Overhanging street',
+      symbol: getStreetSymbol('Overhanging_Street_-_Light_on')
+    },
+    {
+      value: 'Overhanging street and sidewalk',
+      symbol: getStreetSymbol('Light_On_Post_-_Light_on')
+    },
+    {
+      value: 'Trash bin',
+      symbol: getStreetSymbol('Trash_Bin_1')
+    },
+    {
+      value: 'Newspaper',
+      symbol: getStreetSymbol('Newspaper_Vending_Machine')
+    },
+    {
+      value: 'Park bench 1',
+      symbol: getStreetSymbol('Park_Bench_2')
+    }
+  ],
+  visualVariables: [
+    {
+      type: 'rotation',
+      field: 'ROTATION'
+    },
+    {
+      type: 'size',
+      field: 'SIZE',
+      axis: 'height'
+    }
+  ]
+};
+
+
+const buildings = new SceneLayer({
+  url: 'https://tiles.arcgis.com/tiles/z2tnIkrLQ2BRzr6P/arcgis/rest/services/philadelphia_Bldgs_text_untex/SceneServer',
+  elevationInfo: {
+    mode: 'absolute-height',
+    offset: -6
+  }
+})
+
+map.add(buildings);
+
+view.ui.add('extra', 'bottom-right');
+
+const graphicsLayer = new GraphicsLayer({
+  elevationInfo: {mode: 'on-the-ground'}
 });
-document.getElementById("extruded").addEventListener("click", function (event) {
-  footprintBuildings.visible = false;
-  extrudedBuildings.visible = true;
-});*/
+view.map.add(graphicsLayer);
+
+const treeBtn = document.getElementById('tree');
+const busBtn = document.getElementById('bus');
+
+view
+  .when(function () {
+    // This sample uses the SketchViewModel to add points to a
+    // GraphicsLayer. The points have 3D glTF models as symbols.
+    const sketchVM = new SketchViewModel({
+      layer: graphicsLayer,
+      view: view
+    });
+
+    treeBtn?.addEventListener('click', function () {
+      // reference the relative path to the glTF model
+      // in the resource of an ObjectSymbol3DLayer
+      sketchVM.pointSymbol = {
+        type: 'point-3d',
+        symbolLayers: [
+          {
+            type: 'object',
+            resource: {
+              href:
+                'https://static.arcgis.com/arcgis/styleItems/RealisticTrees/web/resource/AcerPlatanoides.json'
+            }
+          }
+        ]
+      } as any;
+      sketchVM.create('point');
+      this.classList.add('esri-button--secondary');
+    });
+
+    busBtn?.addEventListener('click', function () {
+      // reference the relative path to the glTF model
+      // in the resource of an ObjectSymbol3DLayer
+      sketchVM.pointSymbol = {
+        type: 'point-3d',
+        symbolLayers: [
+          {
+            type: 'object',
+            resource: {
+              href:
+                'https://static.arcgis.com/arcgis/styleItems/RealisticTransportation/web/resource/Bus.json'
+            }
+          }
+        ]
+      } as any;
+      sketchVM.create('point');
+      this.classList.add('esri-button--secondary');
+    });
+
+    sketchVM.on('create', function (event) {
+      if (event.state === 'complete') {
+        sketchVM.update(event.graphic);
+      }
+    });
+  })
+  .catch(console.error);
