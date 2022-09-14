4b0a2a2a1ff9b550031bd4961bdced509a05118a
Staijn1 <stein@jnkr.eu>
2022-09-14 16:17:42 +0200

    Added a tree and windturbine map

--- ArcgisWeb/tryout/src/pages/gebouwen.ts
+++ ArcgisWeb/tryout/src/pages/gebouwen.ts
@@ -4,7 +4,6 @@ import SketchViewModel from '@arcgis/core/widgets/Sketch/SketchViewModel';
 import SceneLayer from '@arcgis/core/layers/SceneLayer';
 import esriConfig from '@arcgis/core/config.js';
 import {apiKey} from '../vite-env';
-import VectorTileLayer from '@arcgis/core/layers/VectorTileLayer';
 import ElevationLayer from '@arcgis/core/layers/ElevationLayer';
 
 esriConfig.apiKey = apiKey;
@@ -15,14 +14,19 @@ const elevation = new ElevationLayer(
 
 const buildings = new SceneLayer({
   url: 'https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/BAG_3D_WGS/SceneServer',
-  elevationInfo: {
-    mode: 'absolute-height',
-    offset: -6
-  }
+})
+
+const trees = new SceneLayer({
+  url: "https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/3D_Bomen_WGS/SceneServer",
+})
+
+const windTurbines = new SceneLayer({
+    url: "https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/3D_Windturbines_WGS/SceneServer/",
 })
 
 const map = new WebScene({
-  basemap: 'satellite',
+  basemap: 'hybrid',
+  layers: [buildings, trees, windTurbines],
 } as any);
 
 map.ground.layers.add(elevation);
@@ -34,8 +38,8 @@ const view = new SceneView({
   map: map,
   camera: {
     position: {
-      latitude: 52.377956,
-      longitude: 4.897070,
+      latitude: 51.9851,
+      longitude: 5.8987,
       z: 5000
     },
     tilt: 0,
@@ -43,14 +47,15 @@ const view = new SceneView({
   },
   environment: {
     lighting: {
-      date: new Date('June 15, 2015 16:00:00 CET'),
-      directShadowsEnabled: true,
-      ambientOcclusionEnabled: true
+      date: new Date(),
+      directShadowsEnabled: true
+    },
+    atmosphere: {
+      quality: "low"
     }
   }
 });
 
-map.add(buildings);
 
 
 view
