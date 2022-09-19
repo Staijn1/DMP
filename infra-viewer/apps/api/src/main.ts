cd73a051e440ec952fa19fd17ad172bc094877ed
Staijn1 <stein@jnkr.eu>
2022-09-19 15:03:24 +0200

    Setup swagger

--- infra-viewer/apps/api/src/main.ts
+++ infra-viewer/apps/api/src/main.ts
@@ -5,17 +5,34 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
+import {json, urlencoded} from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  app.setGlobalPrefix('api');
+  app.enableCors()
+
+  // read the version from package.json
+  const config = new DocumentBuilder()
+    .setTitle('Infraviewer API')
+    .setDescription('The API behind the Infraviewer')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.use(json({ limit: '1mb' }));
+  app.use(urlencoded({ extended: true, limit: '1mb' }));
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/api`
   );
 }
 
