8614fc77a991f1c9c6b58b27079f0b3328d3df93
Staijn1 <stein@jnkr.eu>
2022-09-20 15:15:06 +0200

    Added UI elements

--- infra-viewer/apps/website/src/app/shared/components/arcgis-map/arcgis-map.component.ts
+++ infra-viewer/apps/website/src/app/shared/components/arcgis-map/arcgis-map.component.ts
@@ -2,6 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import WebScene from '@arcgis/core/WebScene';
 import SketchViewModel from '@arcgis/core/widgets/Sketch/SketchViewModel';
 import SceneView from '@arcgis/core/views/SceneView';
+import ElevationProfile from '@arcgis/core/widgets/ElevationProfile';
+import Search from '@arcgis/core/widgets/Search';
+import LayerList from '@arcgis/core/widgets/LayerList';
+import Expand from '@arcgis/core/widgets/Expand';
 
 @Component({
   selector: 'app-arcgis-map',
@@ -38,8 +42,7 @@ export class ArcgisMapComponent implements OnInit {
         }
       }
     });
-
-
+    this.createUI(view);
     view
       .when(() => {
         view.goTo({
@@ -63,5 +66,46 @@ export class ArcgisMapComponent implements OnInit {
         });
       })
       .catch(console.error);
+  }
+
+  private createUI(view: SceneView): void {
+    const searchWidget = new Search({
+      view: view,
+      container: document.createElement('div'),
+    });
+
+    const layerList = new LayerList({
+      view: view
+    })
+
+    const layerlistExpand = new Expand({
+      view: view,
+      content: layerList,
+    })
+
+    const elevationProfile = new ElevationProfile({
+      view: view,
+      profiles: [{
+        type: 'ground' // first profile line samples the ground elevation
+      }, {
+        type: 'view' // second profile line samples the view and shows building profiles
+      }],
+      // hide the select button
+      // this button can be displayed when there are polylines in the
+      // scene to select and display the elevation profile for
+      visibleElements: {
+        selectButton: false
+      }
+    });
+
+    const elevationProfileExpand = new Expand({
+      view: view,
+      content: elevationProfile
+    });
+
+
+    view.ui.add(elevationProfileExpand, 'top-left');
+    view.ui.add(layerlistExpand, 'top-left');
+    view.ui.add(searchWidget, 'top-right');
   }
 }
