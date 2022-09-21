097e352530f2ada0cf43aebf11faa26f2405aa19
Staijn1 <stein@jnkr.eu>
2022-09-21 10:45:54 +0200

    Migration run

--- infra-viewer/apps/api/src/app/app.module.ts
+++ infra-viewer/apps/api/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import {SystemModule} from './System/system.module';
 
 @Module({
   imports: [SystemModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
