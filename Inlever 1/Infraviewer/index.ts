704e4789af73748e13cf372421475c86a9c62769
Staijn1 <stein@jnkr.eu>
2022-09-29 09:41:49 +0200

    First deadline results

---
+++ Inlever 1/Infraviewer/index.ts
@@ -0,0 +1,240 @@
+import WebScene from '@arcgis/core/WebScene';
+import SceneView from '@arcgis/core/views/SceneView';
+import SketchViewModel from '@arcgis/core/widgets/Sketch/SketchViewModel';
+import SceneLayer from '@arcgis/core/layers/SceneLayer';
+import esriConfig from '@arcgis/core/config.js';
+import ElevationLayer from '@arcgis/core/layers/ElevationLayer';
+import LayerList from '@arcgis/core/widgets/LayerList';
+import Search from '@arcgis/core/widgets/Search';
+import Expand from '@arcgis/core/widgets/Expand';
+import ElevationProfile from '@arcgis/core/widgets/ElevationProfile';
+import GeoJSONLayer from '@arcgis/core/layers/GeoJSONLayer';
+import WebStyleSymbol from '@arcgis/core/symbols/WebStyleSymbol';
+import RendererProperties = __esri.RendererProperties;
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+import {apiKey} from './src/vite-env';
+
+esriConfig.apiKey = apiKey;
+
+
+/*************************************************
+ *                  Layers
+ ************************************************/
+const elevation = new ElevationLayer(
+  {
+    url: 'https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Elevation_3D_WGS/ImageServer',
+    id: 'elevation'
+  }
+)
+
+const buildings = new SceneLayer({
+  url: 'https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/BAG_3D_WGS/SceneServer',
+  id: 'buildings',
+  featureReduction: {type: 'selection'},
+  renderer: {
+    type: 'simple',
+    symbol: {
+      type: 'mesh-3d',  // autocasts as new MeshSymbol3D()
+      symbolLayers: [{
+        type: 'fill',  // autocasts as new FillSymbol3DLayer()
+        material: {color: '#afbcf8'}
+      }]
+    }
+  } as RendererProperties
+})
+
+const trees = new SceneLayer({
+  id: 'trees',
+  screenSizePerspectiveEnabled: true,
+  url: 'https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/3D_Bomen_WGS/SceneServer',
+  featureReduction: {type: 'selection'},
+})
+
+const windTurbines = new SceneLayer({
+  id: 'windTurbines',
+  url: 'https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/3D_Windturbines_WGS/SceneServer/',
+})
+
+const speeltoestellen = new GeoJSONLayer({
+  id: 'speeltoestellen',
+  url: 'https://geo.arnhem.nl/arcgis/rest/services/OpenData/Spelenkaart/MapServer/1/query?outFields=*&where=1%3D1&f=geojson',
+  title: 'Speeltoestellen',
+  renderer: {
+    type: 'unique-value', // autocasts as new UniqueValueRenderer()
+    field: 'TYPE_SPEELTOESTEL',
+    uniqueValueInfos: [
+      {
+        value: 'Schommel (st)',
+        symbol: new WebStyleSymbol({
+          name: 'Swing',
+          styleName: 'EsriRecreationStyle'
+        })
+      },
+      {
+        value: 'Speeltoestel derden (st)',
+        symbol: new WebStyleSymbol({
+          name: 'Slide',
+          styleName: 'EsriRecreationStyle'
+        })
+      },
+      {
+        value: 'Wip (st)',
+        symbol: new WebStyleSymbol({
+          name: 'Teeter_Totter',
+          styleName: 'EsriRecreationStyle'
+        })
+      },
+      {
+        value: 'Klimrek (st)',
+        symbol: new WebStyleSymbol({
+          name: 'Climbing_Frame',
+          styleName: 'EsriRecreationStyle'
+        })
+      }
+    ]
+  } as RendererProperties
+})
+
+const trafficSigns = new FeatureLayer({
+  id: 'trafficSigns',
+  url: 'https://services.arcgis.com/nSZVuSZjHpEZZbRo/arcgis/rest/services/Verkeersborden_NDW/FeatureServer/0',
+  screenSizePerspectiveEnabled: true,
+  featureReduction: {
+    type: 'selection'
+  }
+});
+
+const banken = new GeoJSONLayer({
+  url: 'https://geo.arnhem.nl/arcgis/rest/services/OpenData/Banken/MapServer/0/query?outFields=*&where=1%3D1&f=geojson',
+  title: 'Banken',
+  renderer: {
+    type: 'simple',
+    symbol: new WebStyleSymbol({
+      name: 'Park_Bench_2',
+      styleName: 'EsriRealisticStreetSceneStyle'
+    })
+  } as RendererProperties
+})
+
+const trolleymasten = new GeoJSONLayer({
+  url: 'https://geo.arnhem.nl/arcgis/rest/services/OpenData/Trolleymasten/MapServer/0/query?outFields=*&where=1%3D1&f=geojson',
+  title: 'Trolleymasten',
+  renderer: {
+    type: 'simple',
+    symbol: new WebStyleSymbol({
+      // Todo make this a proper trolley pole
+      name: 'Overhanging_Street_and_Sidewalk_-_Light_on',
+      styleName: 'EsriRealisticStreetSceneStyle'
+    })
+  } as RendererProperties
+})
+
+const afvalbakken = new GeoJSONLayer({
+  url: 'https://geo.arnhem.nl/arcgis/rest/services/OpenData/Afvalbakken/MapServer/0/query?outFields=*&where=1%3D1&f=geojson',
+  id: 'afvalbakken',
+  title: 'Afvalbakken',
+  renderer: {
+    type: 'simple',
+    symbol: new WebStyleSymbol({
+      name: 'Trash_Bin_2',
+      styleName: 'EsriRealisticStreetSceneStyle'
+    })
+  } as RendererProperties
+})
+
+const map = new WebScene({
+  basemap: 'hybrid',
+  layers: [buildings, trees, windTurbines, banken, afvalbakken, trolleymasten, trafficSigns, speeltoestellen]
+} as any);
+
+map.ground.layers.add(elevation);
+
+
+/*************************************************
+ *                  View
+ ************************************************/
+// Create the view
+const view = new SceneView({
+  container: 'viewDiv',
+  map: map,
+  environment: {
+    lighting: {
+      date: new Date(),
+      directShadowsEnabled: true
+    },
+    atmosphere: {
+      quality: 'low'
+    }
+  }
+});
+
+
+view
+  .when(function () {
+
+    view.goTo({
+      position: {
+        latitude: 51.964370,
+        longitude: 5.910011,
+        z: 2500
+      },
+      tilt: 30,
+      heading: 0
+    }).then();
+
+    const sketchVM = new SketchViewModel({
+      view: view
+    });
+
+    sketchVM.on('create', function (event) {
+      if (event.state === 'complete') {
+        sketchVM.update(event.graphic);
+      }
+    });
+  })
+  .catch(console.error);
+
+/*************************************************
+ *                 UI
+ ************************************************/
+
+const searchWidget = new Search({
+  view: view,
+  container: document.createElement('div'),
+});
+
+const layerList = new LayerList({
+  view: view
+})
+
+const layerlistExpand = new Expand({
+  view: view,
+  content: layerList,
+})
+const elevationProfile = new ElevationProfile({
+  view: view,
+  profiles: [{
+    type: 'ground' // first profile line samples the ground elevation
+  }, {
+    type: 'view' // second profile line samples the view and shows building profiles
+  }],
+  // hide the select button
+  // this button can be displayed when there are polylines in the
+  // scene to select and display the elevation profile for
+  visibleElements: {
+    selectButton: false
+  }
+});
+const elevationProfileExpand = new Expand({
+  view: view,
+  content: elevationProfile
+});
+
+view.ui.add(elevationProfileExpand, 'top-left');
+view.ui.add(layerlistExpand, {
+  position: 'top-left'
+});
+
+view.ui.add(searchWidget, {
+  position: 'top-right'
+});
---
+++ Inlever 1/README.txt
@@ -0,0 +1,13 @@
+DRP.pdf 
+	Design Research Paper
+SRS.pdf 
+	Software Requirements Specification, mijn deliverable voor de define fase.
+	Hier worden de eisen in vastgesteld voor de applicatie
+
+Experiment 1 Infraviewer.pdf
+	In detail beschrijving van het uitgevoerde experiment. 
+	De conclusie van het experiment is ook terug te lezen in het DRP
+
+Map infraviewer
+	Dit bevat de sourcecode van het expirement. Je kan dit niet direct openen.
+	Het resultaat van het experiment is te zien op https://dmpexperiment1.netlify.app/
