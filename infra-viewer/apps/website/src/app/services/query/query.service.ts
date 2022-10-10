7fa00f994ed8d0506d75585acd09e1dcd6aeb1bb
Staijn1 <stein@jnkr.eu>
2022-10-10 16:33:40 +0200

    Code cleanup

--- infra-viewer/apps/website/src/app/services/query/query.service.ts
+++ infra-viewer/apps/website/src/app/services/query/query.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import Query from '@arcgis/core/rest/support/Query';
-import SceneLayer from '@arcgis/core/layers/SceneLayer';
 import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
 
 @Injectable({
@@ -8,32 +7,16 @@ import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
 })
 export class QueryService {
 
-  public async queryOnTime(layer: __esri.FeatureLayer, from: Date | string, to: Date | string, field: string): Promise<__esri.FeatureSet> {
-    // If from and to are strings, convert them to Date objects
-    if (typeof from === 'string') {
-      from = new Date(from);
-    }
-    if (typeof to === 'string') {
-      to = new Date(to);
-    }
-
-    const query = layer.createQuery();
-    query.outFields = ['*'];
-    query.where = `${field} >= ${from.getTime()} AND ${field} <= ${to.getTime()}`;
-
-    return layer.queryFeatures(query);
-  }
-
   queryOnLocation(mapPoint: __esri.Point, layers: __esri.Collection<__esri.Layer>): Promise<__esri.FeatureSet[]> {
     const query = new Query();
     query.geometry = mapPoint;
     query.distance = 100;
     query.units = 'meters';
-    query.spatialRelationship = 'intersects';
     query.outFields = ['*'];
     query.returnGeometry = true;
 
     const querySupportedLayers = layers.filter(layer => layer.type === 'feature' || layer.type === 'geojson');
+    console.log(querySupportedLayers.map(layer => layer.title));
     // Create an array of promises, one for each layer
     const promises = querySupportedLayers.map(layer => (layer as FeatureLayer).queryFeatures(query));
     // Return a promise that resolves when all promises in the array have resolved
