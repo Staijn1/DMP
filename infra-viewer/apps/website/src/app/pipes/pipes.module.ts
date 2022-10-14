006c6347f946f7e0edb2a0dc0eef9521e14c8d90
Staijn1 <stein@jnkr.eu>
2022-10-14 10:19:48 +0200

    Expanded config page

---
+++ infra-viewer/apps/website/src/app/pipes/pipes.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/core';
+import {CastPipe} from './cast/cast.pipe';
+
+@NgModule({
+  declarations: [CastPipe],
+  exports: [CastPipe],
+})
+export class PipesModule {
+}
