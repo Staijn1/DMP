4cb411d7e17f5b2624f5a1a4def2d292639628fc
Staijn1 <stein@jnkr.eu>
2022-10-17 12:08:50 +0200

    Revamp welcome page

--- infra-viewer/apps/website/src/app/pages/welcome/welcome-page.component.ts
+++ infra-viewer/apps/website/src/app/pages/welcome/welcome-page.component.ts
@@ -1,15 +1,21 @@
 import {Component} from '@angular/core';
-import {AuthenticationService} from '../../services/authentication-service/authentication.service';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.css'],
+  styleUrls: ['./welcome-page.component.scss'],
 })
 export class WelcomePageComponent {
-  constructor(private authService: AuthenticationService) {
+  constructor() {
   }
-  startLogin(): void {
-      this.authService.startLogin();
+
+  /**
+   * Uses the environments api url to get the url to the publicly hosted images
+   * It does this by removing /api from the url and replacing it with {imagename}
+   * @param imagename
+   */
+  getSrc(imagename: string): string {
+    return environment.api.replace('/api', `/${imagename}`);
   }
 }
