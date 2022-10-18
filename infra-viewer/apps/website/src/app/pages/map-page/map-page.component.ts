3482664a6b542d9572614777e58c42293325add5
Staijn1 <stein@jnkr.eu>
2022-10-18 22:30:08 +0200

    Make a list of queried features

--- infra-viewer/apps/website/src/app/pages/map-page/map-page.component.ts
+++ infra-viewer/apps/website/src/app/pages/map-page/map-page.component.ts
@@ -1,9 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {MapEventHandlerService} from '../../services/map-event-handler/map-event-handler.service';
+import {Subscription} from 'rxjs';
+import FeatureSet from '@arcgis/core/rest/support/FeatureSet';
+import FeatureLayerProperties = __esri.FeatureLayerProperties;
 
 @Component({
   selector: 'app-map-page',
   templateUrl: './map-page.component.html',
   styleUrls: ['./map-page.component.scss'],
 })
-export class MapPageComponent {
+export class MapPageComponent implements OnDestroy {
+  private featureSubscription: Subscription;
+  results: { featureSet: FeatureSet, layer: FeatureLayerProperties }[] | null = null;
+
+  constructor(private readonly eventHandler: MapEventHandlerService) {
+    this.featureSubscription = eventHandler.queredFeatures$.subscribe(results =>this.results = results)
+  }
+
+  ngOnDestroy(): void {
+    this.featureSubscription.unsubscribe();
+  }
 }
