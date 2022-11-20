ae54999eb2d66e9cbf7f6bf23941c87f32161b92
Staijn1 <stein@jnkr.eu>
2022-11-20 21:58:31 +0100

    Update authentication.service.ts

--- infra-viewer/apps/website/src/app/services/authentication/authentication.service.ts
+++ infra-viewer/apps/website/src/app/services/authentication/authentication.service.ts
@@ -2,19 +2,30 @@ import {Injectable} from '@angular/core';
 import esriId from '@arcgis/core/identity/IdentityManager';
 import {Router} from '@angular/router';
 import {environment} from '../../../environments/environment';
-import Portal from '@arcgis/core/portal/Portal';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  private static credential: __esri.Credential;
   private readonly appId = 'jpL480B69UHL0NWO';
   private readonly portalURL = environment.portalURL;
   private readonly portalSharingUrl = this.portalURL + '/sharing/';
+  private _token!: string;
+
 
   get isLoggedIn(): boolean {
-    return AuthenticationService.credential !== undefined;
+    const credential = sessionStorage.getItem('credential');
+    try {
+      const parsed = JSON.parse(credential || '{}');
+      this.registerToken(parsed.token, parsed.expires);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  get token(): string {
+    return this._token;
   }
 
   constructor(private router: Router) {
@@ -34,14 +45,19 @@ export class AuthenticationService {
     // Get the token from the url, it is in the format #access_token=token&expires_in=3600&token_type=Bearer
     const token = window.location.hash.split('&')[0].split('=')[1];
     const expiration = window.location.hash.split('&')[1].split('=')[1];
+    this.registerToken(token, expiration);
+    esriId.getCredential(this.portalSharingUrl).then((credential) => {
+      sessionStorage.setItem('credential', JSON.stringify(credential));
+      return this.router.navigateByUrl('/map');
+    }).catch(e => console.error(e))
+  }
+
+  private registerToken(token: string, expiration: string) {
+    this._token = token;
     esriId.registerToken({
       server: this.portalSharingUrl,
       token: token,
       expires: isNaN(Number(expiration)) ? new Date().getTime() + (3600 * 1000) : Number(expiration)
     });
-    esriId.getCredential(this.portalSharingUrl).then((credential) => {
-      AuthenticationService.credential = credential
-      return this.router.navigateByUrl('/map');
-    }).catch(e => console.error(e))
   }
 }
