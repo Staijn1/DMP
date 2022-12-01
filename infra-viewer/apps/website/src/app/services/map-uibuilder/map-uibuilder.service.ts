1faa19223aaa6e276fe5178b339658174af1a8f4
Staijn1 <stein@jnkr.eu>
2022-12-01 16:52:03 +0100

    Sewers on real height! 😲

--- infra-viewer/apps/website/src/app/services/map-uibuilder/map-uibuilder.service.ts
+++ infra-viewer/apps/website/src/app/services/map-uibuilder/map-uibuilder.service.ts
@@ -136,7 +136,6 @@ export class MapUIBuilderService implements OnDestroy {
     }
     view.ui.add([layerlistExpand, fullScreen, legendExpand], 'top-left');
     view.ui.add([this.searchWidget, elevationProfileExpand, daylightExpand, shadowWidget, editor, directLineMeasurement, areaMeasurement], 'top-right');
-    
   }
 
   /**
